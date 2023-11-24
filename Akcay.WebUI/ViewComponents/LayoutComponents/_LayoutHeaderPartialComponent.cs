3388f4ccc30872768b6f10f85c337eec508c2456
Haruncan Atalı <77530565+haruncanatali@users.noreply.github.com>
2023-11-24 11:59:23 +0300

    web-ui-fix

---
+++ Akcay.WebUI/ViewComponents/LayoutComponents/_LayoutHeaderPartialComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Akcay.WebUI.ViewComponents.LayoutComponents;
+
+public class _LayoutHeaderPartialComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }   
+}
